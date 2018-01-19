982759eb40c9db05fc79bfedda5528e0ee4b3457
Erich Mielke <emielke12@gmail.com>
2018-01-18 19:54:05 -0700

    some updates to hw2

--- hw2/MAVequations.py
+++ hw2/MAVequations.py
@@ -5,65 +5,121 @@ import matplotlib.pyplot as plt
 
 class MAVEOM(AirCraftDrawing):
     def __init__(self):
-        M = 2
+        # Animation Class Inheritance
+        AirCraftDrawing.__init__(self)
 
-    def trans_kin(self,u,t,psi,th,phi):
-        ''' Eq. 3.14 '''
+        # Plane Parameters
+        self.M = 2
+        self.g = 9.81
+        self.Jx = 0.01
+        self.Jy = 0.01
+        self.Jz = 0.01
+        self.Jxz = 0.01
+
+    def equations_of_motion(self,x,t,uu):
+        # States
+        pn = x[0]
+        pe = x[1]
+        pd = x[2]
+        u = x[3]
+        v = x[4]
+        w = x[5]
+        phi = x[6]
+        th = x[7]
+        psi = x[8]
+        p = x[9]
+        q = x[10]
+        r = x[11]
+
+        # Inputs
+        fx = uu[0]
+        fy = uu[1]
+        fz = uu[2]
+        l = uu[3]
+        m = uu[4]
+        n = uu[5]
+        
+        # Eq 3.14 Translational Kinematics
         R = np.transpose([[np.cos(th)*np.cos(psi), np.cos(th)*np.sin(psi), -np.sin(th)],
              [np.sin(phi)*np.sin(th)*np.cos(psi) - np.cos(phi)*np.sin(psi), np.sin(phi)*np.sin(th)*np.sin(psi) + np.cos(phi)*np.cos(psi), np.sin(phi)*np.cos(th)],
              [np.cos(phi)*np.sin(th)*np.cos(psi) + np.sin(phi)*np.sin(psi), np.cos(phi)*np.sin(th)*np.sin(psi) - np.sin(phi)*np.cos(psi), np.cos(phi)*np.cos(th)]])
-        dpedt = np.matmul(R,u)
-        return dpedt
-
-    def rot_kin(self,p,t,psi,th,phi):
-        ''' Eq. 3.16 '''
-        R = [[1, np.sin(phi)*np.tan(th), np.cos(phi)*np.tan(th)],
-             [0, np.cos(phi), -np.sin(phi)],
-             [0, np.sin(phi)/np.cos(th), np.cos(phi)/np.cos(th)]]
-        deuldt = np.matmul(R,p)
-        return deuldt
-
-    def trans_dyn(self,f,t,p,q,r,u,v,w,M):
-        ''' Eq. 3.15 '''
+        dpNEDdt = np.matmul(R,[u,v,w])
+        
+        # Eq 3.15 Translational Dynamics
         R = [[r * v - q * w],
              [p * w - r * u],
              [q * u - p * v]]
-        dudt = np.multiply(1.0/M,f) + R
-        return dudt
+        duvwdt = np.multiply(1.0/self.M,[fx,fy,fz]) + R
 
-    def rot_dyn(self,tau,t,p,q,r,Jx,Jy,Jz,Jxz):
-        ''' Eq. 3.17 '''
-        l,m,n = tau
-        g = Jx*Jz - Jxz**2
-        g1 = Jxz*(Jx - Jy + Jz)
-        g2 = Jz * (Jz - Jy) + Jxz**2
-        g3 = Jz / g
-        g4 = Jxz / g
-        g5 = (Jz - Jx) / Jy
-        g6 = Jxz / Jy
-        g7 = ((Jx - Jy) * Jx + Jxz**2)/g
-        g8 = Jx / g
+        # Eq 3.16 Rotational Kinematics
+        R = [[1, np.sin(phi)*np.tan(th), np.cos(phi)*np.tan(th)],
+             [0, np.cos(phi), -np.sin(phi)],
+             [0, np.sin(phi)/np.cos(th), np.cos(phi)/np.cos(th)]]
+        deuldt = np.matmul(R,[p,q,r])
+
+        # Eq 3.17 Rotational Dynamics
+        g = self.Jx*self.Jz - self.Jxz**2
+        g1 = self.Jxz*(self.Jx - self.Jy + self.Jz)
+        g2 = self.Jz * (self.Jz - self.Jy) + self.Jxz**2
+        g3 = self.Jz / g
+        g4 = self.Jxz / g
+        g5 = (self.Jz - self.Jx) / self.Jy
+        g6 = self.Jxz / self.Jy
+        g7 = ((self.Jx - self.Jy) * self.Jx + self.Jxz**2)/g
+        g8 = self.Jx / g
 
         R1 = [[g1 * p * q - g2 * q * r],
               [g5 * p * r - g6 * (p**2 - r**2)],
               [g7 * p * q - g1 * q * r]]
         R2 = [[g3 * l + g4 * n],
-              [m / Jy],
+              [m / self.Jy],
               [g4 * l + g8 * n]]
-        dpdt = np.add(R1,R2)
-        return dpdt
+        dpqrdt = np.add(R1,R2)
 
-# psi = 0.0
-# th = 0.0
-# phi = 0.0
+        xdot = [dpNEDdt[0], dpNEDdt[1], dpNEDdt[2], duvwdt[0], duvwdt[1], duvwdt[2], 
+                deuldt[0], deuldt[1], deuldt[2], dpqrdt[0], dpqrdt[1], dpqrdt[2]]
+        return xdot
 
-# y0 = [1.0, 0.0, 0.0]
 
-# t = np.linspace(0,10,101)
+if __name__ == "__main__":
+    aircraft = AirCraftDrawing()
+    meq = MAVEOM(AirCraftDrawing)
 
-# sol = odeint(pend,y0,t,args=(psi,th,phi))
+    # Initial Conditions
+    pn0 = 0
+    pe0 = 0
+    pd0 = 0
+    u0 = 0
+    v0 = 0
+    w0 = 0
+    phi0 = 0
+    th0 = 0
+    psi0 = 0
+    p0 = 0
+    q0 = 0
+    r0 = 0
 
-# plt.plot(t,sol[:,0])
-# plt.show()
-# B = MAVEOM()
-# print B.fuse_l1
+    x0 = [pn0,pe0,pd0,u0,v0,w0,phi0,th0,psi0,p0,q0,r0] 
+
+    # Inputs
+    fx = 1.0
+    fy = 0.0
+    fz = 0.0
+    l = 0.0
+    m = 0.0
+    n = 0.0
+
+    uu = [fx,fy,fz,l,m,n]
+
+    # Time Vector
+    t = np.linspace(0,10,101)
+
+    # Solve ODE
+    sol = odeint(equations_of_motion,x0,t,args=(uu))
+
+#     # Plot
+#     plt.plot(t,sol[:,0])
+#     plt.show()
+    pos = [sol[:,0],sol[:,1],sol[:,2]]
+    eul = [sol[:,6],sol[:,7],sol[:,8]]
+    meq.input_goal(eul,pos)
--- hw2/airframe.py
+++ hw2/airframe.py
@@ -169,9 +169,9 @@ class AirCraftDrawing():
         plt.draw()
         
     def get_rotation_matrix(self,eul):
-        psi = eul[0] # yaw 
+        psi = eul[2] # yaw 
         th = eul[1] # pitch 
-        phi = eul[2] # roll
+        phi = eul[0] # roll
         R = [[np.cos(th)*np.cos(psi), np.cos(th)*np.sin(psi), -np.sin(th)],
              [np.sin(phi)*np.sin(th)*np.cos(psi) - np.cos(phi)*np.sin(psi), np.sin(phi)*np.sin(th)*np.sin(psi) + np.cos(phi)*np.cos(psi), np.sin(phi)*np.cos(th)],
              [np.cos(phi)*np.sin(th)*np.cos(psi) + np.sin(phi)*np.sin(psi), np.cos(phi)*np.sin(th)*np.sin(psi) - np.sin(phi)*np.cos(psi), np.cos(phi)*np.cos(th)]] # rotation matrix
@@ -194,23 +194,41 @@ class AirCraftDrawing():
 
     # ------------------------------------------------------------------------------#
     # If not using Tkinter but just inputting calculated euler angles and position
-    def input_goal(self,euler,positions):
+    def input_goal(self,euler,positions,single_position = False):
         '''Inputs:
         euler_angles: vector of euler angles in order psi, theta, phi
         positions: vector of positions in order x, y, z'''
-        self.phi = euler[2]
+        self.phi = euler[0]
         self.th = euler[1]
-        self.psi = euler[0]
+        self.psi = euler[2]
         self.x = positions[0]
         self.y = positions[1]
         self.z = positions[2]
-        self.slider_goal()
+
+        if single_position == True:
+            self.slider_goal()
+        else:
+            self.calculated_trajectory()
+
+    def calculated_trajectory(self):
+        self.ang = [self.phi,self.th,self.psi]
+        self.pos = [self.x,self.y,self.z]
+        self.show_animation()
+
+        # Now set init as current
+        self.init_psi = deepcopy(self.psi)
+        self.init_th = deepcopy(self.th)
+        self.init_phi = deepcopy(self.phi)
+        self.init_x = deepcopy(self.x)
+        self.init_y = deepcopy(self.y)
+        self.init_z = deepcopy(self.z)
+
 
     def slider_goal(self):
         # Desired euler angles and positions here (from sliders)
-        self.ang = [np.linspace(self.init_psi,self.psi,self.n_frames),
+        self.ang = [np.linspace(self.init_phi,self.phi,self.n_frames),
                     np.linspace(self.init_th,self.th,self.n_frames),
-                    np.linspace(self.init_phi,self.phi,self.n_frames)]
+                    np.linspace(self.init_psi,self.psi,self.n_frames)]
         self.pos = [np.linspace(self.init_x,self.x,self.n_frames),
                     np.linspace(self.init_y,self.y,self.n_frames),
                     np.linspace(self.init_z,self.z,self.n_frames)]
