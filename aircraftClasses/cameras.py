19f26897393aad4ce02002cd3c3cd3c76929499e
Erich Mielke <emielke12@gmail.com>
2018-04-11 20:48:48 -0600

    made some progress on hw13

--- aircraftClasses/cameras.py
+++ aircraftClasses/cameras.py
@@ -8,32 +8,54 @@ class Cameras(pathPlanner):
     def __init__(self,va0 = 30.0,model=1):
         pathPlanner.__init__(self,va0,model)
 
-        self.k_az = 1.0
-        self.k_el = 1.0
+        self.k_az = 1.5
+        self.k_el = 1.5
+        self.az0 = 0
+        self.el0 = -np.pi/2
+        self.cam_pix = 480
+        self.cam_fov = 10 * np.pi / 180.0
+        self.cam_f = 0.5 * self.cam_pix / np.tan(self.cam_fov/2)
+        self.target_size = 2.0
+        self.az_lim = 180 * np.pi / 180.0
+        self.el_lim = 180 * np.pi / 180.0
 
-    def point_gimbal(self,p_obj,p_mav,phi,th,psi,alpha_az,alpha_el):
+    def point_gimbal(self,p_obj,p_mav,phi,th,psi,lcd,Rcg,Rgb):
         li_d = np.subtract(p_obj,p_mav)
-        Rb_i = self.get_rotation_matrix([phi,th,psi])
+        Rb_i = self.get_rotation_matrix(phi,th,psi)
         lb_d = 1/np.linalg.norm(li_d) * np.matmul(Rb_i,li_d)
 
+#         RR = np.matmul(Rgb,Rcg)
+#         lb_d = np.matmul(RR,lcd)
         alphac_az = np.arctan(lb_d[1]/lb_d[0])
         alphac_el = np.arcsin(lb_d[2])
 
-        u_az = self.k_az * (alphac_az - alpha_az)
-        u_el = self.k_el * (alphac_el - alpha_el)
+        u_az = self.k_az * (alphac_az - deepcopy(self.az0))
+        u_el = self.k_el * (alphac_el - deepcopy(self.el0))
 
-        return u_az,u_el,alphac_az,alphac_el
+        return u_az,u_el
 
-    def geolocation(self,p_obj,p_mav,phi,th,psi,az,el):
+    def geolocation(self,p_obj,p_mav,phi,th,psi):
         Rcg = [[0,0,1],
                [1,0,0],
                [0,1,0]]
-        Rbi = self.get_rotation_matrix([phi,th,psi])
-        u_az,u_el,azc,elc = self.point_gimbal(p_obj,p_mav,phi,th,psi,az,el)
-        Rgb = self.rot_b_to_g(azc,elc)
+        Rbi = self.get_rotation_matrix(phi,th,psi)
+        Rgb = self.rot_b_to_g(deepcopy(self.az0),deepcopy(self.el0))
+        RR = np.matmul(np.matmul(Rbi,Rgb),Rcg)
+        epsx,epsy,epss = self.camProj(p_obj,p_mav,RR)
+        lc_d = np.multiply(1 / np.sqrt(self.cam_f**2 + epsx**2 + epsy**2),[epsx,epsy,self.cam_f])
+        u_az,u_el = self.point_gimbal(p_obj,p_mav,phi,th,psi,lc_d,Rcg,Rgb)
+        self.determineNewAlpha(u_az,u_el)
 
-        varrho = np.dot([0,0,1],li)
-        L = -p_mav[2] / varrho
+        self.drawCamera(epsx,epsy)
+
+#         self.determine_location(p_obj,RR,lc
+
+    def get_rotation_matrix(self,phi,th,psi):
+        R = [[np.cos(th) * np.cos(psi),np.sin(phi) * np.sin(th) * np.cos(psi) - np.cos(phi) * np.sin(psi),np.cos(phi) * np.sin(th) * np.cos(psi) + np.sin(phi) * np.sin(psi)],
+             [np.cos(th) * np.sin(psi),np.sin(phi) * np.sin(th) * np.sin(psi) + np.cos(phi) * np.cos(psi),np.cos(phi) * np.sin(th) * np.sin(psi) - np.sin(phi) * np.cos(psi)],
+             [-np.sin(th), np.sin(phi) * np.cos(th), np.cos(phi) * np.cos(th)]]
+        return R
+
 
     def rot_b_to_g(self,az,el):
         Rbg1 = [[np.cos(az),np.sin(az),0],
@@ -45,6 +67,52 @@ class Cameras(pathPlanner):
         return np.matmul(Rg1g,Rbg1)
         
 
-        
+    def camProj(self,p_obj,p_mav,RR):
+        p_obj_v = np.subtract(p_obj,p_mav)
+        p_obj_c = np.matmul(RR,p_obj_v)
+
+        epsx = self.cam_f * p_obj_c[0] / p_obj_c[2]
+        epsy = self.cam_f * p_obj_c[1] / p_obj_c[2]
+        epss = self.cam_f * self.target_size / p_obj_c[2]
+
+        tmp = self.cam_pix / 2 + epss
+        if epsx < -tmp or epsx > tmp or epsy < -tmp or epsy > tmp:
+            epsx = -9999
+            epsy = -9999
+            epss = 0
+
+        return epsx,epsy,epss
+                             
+    def drawCamera(self,epsx,epsy):
+        if self.t_sim < 2 * self.dt:
+            self.cam_win = pg.GraphicsWindow(size=(800,800))
+            self.cam_win.setWindowTitle('Camera')
+            self.cam_win.setInteractive(True)
+
+            self.cam_plot = self.cam_win.addPlot()
+            self.cam_plot.setXRange(-self.cam_pix/2,self.cam_pix/2,padding=0)
+            self.cam_plot.setYRange(-self.cam_pix/2,self.cam_pix/2,padding=0)
+            self.obj_arrow = pg.ArrowItem(angle=0)
+            self.cam_plot.addItem(self.obj_arrow)
+            self.obj_arrow.setPos(epsx,epsy)
+
+        else:
+            self.obj_arrow.setPos(epsx,epsy)
+            self.obj_arrow.setRotation(0)
+            self.app.processEvents()
+            
+    def determineNewAlpha(self,uaz,uel):
+        self.az0 = deepcopy(self.az0) + uaz * self.dt
+        self.el0 = deepcopy(self.el0) + uel * self.dt
+
+        if self.az0 < -self.az_lim:
+            self.az0 = -self.az_lim
+        elif self.az0 > self.az_lim:
+            self.az0 = self.az_lim
+            
+        if self.el0 < -self.el_lim:
+            self.el0 = -self.el_lim
+        elif self.el0 > self.el_lim:
+            self.el0 = self.el_lim
 
         
---
+++ proj_chp_13.py
@@ -0,0 +1,115 @@
+from aircraftClasses.cameras import *
+import sys,time
+
+def ctrl_c(plane,plane2,x0,wind,model):
+    # Kalman Filter AutoPilot emtpy vectors
+    sols = [[],[],[],[],[],[],[],[],[],[],[],[]]
+
+    # command for kalman autopilot
+    cmd2 = [[],[],[]]
+    counter = 1
+
+    # Command for simplified 1
+    cmd = [0,0,0,100,plane.va0,0] # chidot,chi,hdot,h,va,phi_c
+
+    # Use chapter 9 or kalman filter
+    kalman = False
+    path_type = 'straight'
+
+    try:
+        while True:
+            # Position and commanded Velocity
+            p_9 = [plane.x[0],plane.x[1],plane.x[4]]
+            p_est = [plane2.pn_hat,plane2.pe_hat,plane2.h_hat]
+            # p_est = [x0[0],x0[1],-x0[2]]
+            cmd[4] = 30.0 # va
+            
+            # Maneuver to Run
+            r = [0, 0, 100]
+            q = [1, 0, 0]
+            c = [0,0,100]
+            rho = 1000
+            path_type = 'straight'
+            W = [[0,0,100]]
+
+            # Target Tracking
+            p_obj = [5,5,500]
+            p_mav = x0[0:3]
+            phi,th,psi = x0[6:9]
+            plane.geolocation(p_obj,p_mav,phi,th,psi)
+            time.sleep(0.1)
+
+            # Determine Commands for autopilot
+            if kalman == True:
+                if path_type == 'straight':
+                    cmd[3], cmd[1] = plane.follow_straight_line(r,q,p_est,plane2.chi_hat)
+                    # cmd[3], cmd[1] = plane.follow_straight_line(r,q,p_est,plane2.chi)
+                elif path_type == 'orbit':
+                    cmd[3], cmd[1] = plane.follow_orbit(c,rho,p_est,plane2.chi_hat)
+                    # cmd[3], cmd[1] = plane.follow_orbit(c,rho,p_est,plane2.chi)
+            else:
+                if path_type == 'straight':
+                    cmd[3], cmd[1] = plane.follow_straight_line(r,q,p_9,plane.x[2])
+                elif path_type == 'orbit':
+                    cmd[3], cmd[1] = plane.follow_orbit(c,rho,p_9,plane.x[2])
+
+            # Keep track of commanded course angle, height, and airspeed
+            cmd2[0].append(cmd[3])
+            cmd2[1].append(cmd[4])
+            cmd2[2].append(cmd[1])
+
+            # Autopilot Kalman Filter states
+            plane2.altitude_hold(plane2.x_hat,cmd[3],plane2.dt)
+            plane2.airspeed_throttle(plane2.x_hat,cmd[4],plane2.dt)
+            plane2.course_hold(plane2.x_hat,cmd[1],plane2.dt,plane2.chi_hat)
+
+            # Run ODE
+            plane.simulate_states(wind[0:3],cmd,model)
+            fx,fy,fz,l,m,n,va_,alpha_,beta_,wn,we,wd = plane2.force_calc(x0,plane2.deltas,wind)
+            sol = odeint(plane2.eom,x0,[0.0,plane2.dt],args=(fx,fy,fz,l,m,n))
+            plane2.ekf(sol[-1],[fx,fy,fz],[wn,we,wd],counter,plot = False)
+
+            # Update Time
+            plane.t_sim += plane.dt
+            plane2.t_sim += plane2.dt
+            counter += 1
+
+            # Plot
+            commands = [cmd[1],cmd[3],cmd[4]]
+            states = [plane.x[2],plane.x[4],plane.x[6]]
+            autostates = [plane2.chi_hat,plane2.h_hat,plane2.Va_hat]
+            labels = [u'\u03C7 (rad)','h (m)','Va (m/s)'] # chi, height, airspeed
+
+
+#             if kalman == True:
+#                 plane.waypoint_plot(W,p_est)
+#             else:
+#                 plane.waypoint_plot(W,p_9)
+            
+            # Update initial state
+            x0 = sol[-1,:]
+
+    except KeyboardInterrupt:
+        sys.exit()
+
+if __name__ == "__main__":    
+    # Initial Conditions
+    va0 = 30.0
+    x0 = [0,0,-100,va0,0,0,0,0,0,0,0,0]
+    model = 1
+    
+    # Initial Wind Vector
+    # wind = [3,-3,0,0,0,0]
+    wind = [0,0,0,0,0,0]
+
+    # Trim
+    trim = [x0[3],0,5e60]
+    
+    # Instantiate Class
+    plane = Cameras(va0,model)
+    plane.x[4] = -x0[2]
+    plane2 = kalmanFilter(x0,trim)
+    plt.close('all')
+
+    #------------------------ RUNS UNTIL CTRL-C---------------------------------------#
+    ctrl_c(plane,plane2,x0,wind,model)
